7dc71e4d9087490e9a87048fc41ee40ff60a3fca
rezan21 <reza.naghshineh@gmail.com>
2019-08-28 03:54:35 +0100

    Updates

---
+++ precache-manifest.64b91cfb7c070bdde172a6923b02617c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d56d9b75bbd11bcc0b41c619b87bb4f",
+    "url": "/GithubFinder/index.html"
+  },
+  {
+    "revision": "250ca22c06e74eb2f6ba",
+    "url": "/GithubFinder/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "b46d6f0165bc9cbd7cb8",
+    "url": "/GithubFinder/static/js/2.c9245165.chunk.js"
+  },
+  {
+    "revision": "250ca22c06e74eb2f6ba",
+    "url": "/GithubFinder/static/js/main.cbe3928d.chunk.js"
+  },
+  {
+    "revision": "8f687fd1b1342b24abb4",
+    "url": "/GithubFinder/static/js/runtime~main.ac976cf2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/GithubFinder/static/media/logo.5d5d9eef.svg"
+  }
+]);
